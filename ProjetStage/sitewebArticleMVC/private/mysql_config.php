98b4668a86045ec863671495881568e0cb28b204
OROU-GUIDOU <amirathfarah@gmail.com>
2023-01-16 20:30:28 +0100

    suppression du dossier

--- ProjetStage/sitewebArticleMVC/private/mysql_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("MYSQL_HOST", "localhost");
-define("MYSQL_PORT", "8889");
-define("MYSQL_DB", "amirath_1");
-define("MYSQL_USER", "root");
-define("MYSQL_PASSWORD", "root");
-define("MYSQL_CHARSET", "utf8mb4");
